9f065f61b8c67de9276bcd3dc9a0e00ba83f6c7c
Paul Lewallen <paullewallencom@users.noreply.github.com>
2021-01-01 10:32:59 -0800

    I

---
+++ _/Section 05/hooks-todos-END-SECTION-4/src/index.js
@@ -0,0 +1,58 @@
+import React, { useContext, useReducer, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import * as serviceWorker from "./serviceWorker";
+import TodosContext from "./context";
+import todosReducer from "./reducer";
+
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const useAPI = endpoint => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await axios.get(endpoint);
+    setData(response.data);
+  };
+
+  return data;
+};
+
+const App = () => {
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(todosReducer, initialState);
+  const savedTodos = useAPI("https://todos-api-nuquyjkqpx.now.sh/todos");
+
+  useEffect(
+    () => {
+      dispatch({
+        type: "GET_TODOS",
+        payload: savedTodos
+      });
+    },
+    [savedTodos]
+  );
+
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoForm />
+      <TodoList />
+    </TodosContext.Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
